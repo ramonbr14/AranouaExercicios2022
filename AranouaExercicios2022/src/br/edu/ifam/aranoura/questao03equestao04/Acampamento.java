2f5f65f26d7eb354d250f6e70f2671e5483b208e
Ramonbr14 <ramonbr14@gmail.com>
2022-09-10 11:49:31 -0400

    Criada As classes acampamento e Principal acampamento / Falta gerar a classe separaGrupo()

--- AranouaExercicios2022/src/br/edu/ifam/aranoura/questao03equestao04/Acampamento.java
+++ AranouaExercicios2022/src/br/edu/ifam/aranoura/questao03equestao04/Acampamento.java
@@ -1,5 +1,89 @@
 package br.edu.ifam.aranoura.questao03equestao04;
 
-public class Acampamento {
+import javax.swing.JOptionPane;
 
+public class Acampamento {
+	//Valiable Class
+	private String nome;
+	private char equipe;
+	private int idade;
+	
+	String message = "";
+	String title = "";
+	
+	//Construtores //Constructors
+	public Acampamento() {
+		super();
+	}
+
+	public Acampamento(String nome, char equipe, int idade) {
+		super();
+		this.nome = nome;
+		this.equipe = equipe;
+		this.idade = idade;
+	}
+
+
+	public Acampamento(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+
+	//Manual Metods
+	
+	public void imprimir(Acampamento aluno){
+		separarGrupo(aluno);
+		
+		message = "A Princesa Guerreira: "+aluno.nome+" tem "+aluno.idade+" de idade";
+		title = "Acampamento da Amazonas";
+		JOptionPane.showConfirmDialog(null, message, title, 0, JOptionPane.WARNING_MESSAGE);
+	}
+	
+	public void separarGrupo(Acampamento aluno){
+		if(aluno.getIdade()>21) {
+			aluno.setEquipe("C");
+		}else if(aluno.getIdade()>) {
+			
+		}else if(aluno.getIdade()>) {
+			
+		}else if(aluno.getIdade()>)
+	}else 
+		
+	}
+
+
+	public Acampamento cadastraAcampamento(Acampamento aluno){
+		return aluno;
+	}
+
+
+	//getter and setter
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public char getEquipe() {
+		return equipe;
+	}
+
+	public void setEquipe(String string) {
+		string = string;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	
+	
 }
---
+++ AranouaExercicios2022/src/br/edu/ifam/aranoura/questao03equestao04/PrincipalAcampamento.java
@@ -0,0 +1,17 @@
+package br.edu.ifam.aranoura.questao03equestao04;
+
+public class PrincipalAcampamento {
+	
+		Acampamento aluno1 = new Acampamento("Rainha Elsa",12);
+		Acampamento aluno2 = new Acampamento("Princesa Anna",12);
+		Acampamento aluno3 = new Acampamento("Princesa Rapunzel",12);
+		Acampamento aluno4 = new Acampamento("Rainha Eleonor",12);
+		Acampamento aluno5 = new Acampamento("Rainha Astrid",12);
+		Acampamento aluno6 = new Acampamento("Princesa Merida",12);
+		Acampamento aluno7 = new Acampamento("Princesa Diana",12);
+		
+		
+		
+		
+	
+}
